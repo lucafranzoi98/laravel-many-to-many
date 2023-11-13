c94e687588fa7b4a5c1d99ab357555c69e3c58a9
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-13 15:02:34 +0100

    create type model, migration and seeder

---
+++ app/Models/Type.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+Use Illuminate\Support\Str;
+
+class Type extends Model
+{
+    use HasFactory;
+}
---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $types = ['Frontend', 'Backend', 'API', 'Database'];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($new_type->name, '-');
+            $new_type->save();
+        }
+    }
+}

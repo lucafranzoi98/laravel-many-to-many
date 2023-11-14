c474e0111ed6e9b422b1a21b8cadc5b3f6a0d4cd
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-14 14:28:09 +0100

    Technology and pivot
    Create technology model, migration and seeder
    Create pivot table

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -21,5 +22,10 @@ class Project extends Model
     public function type(): BelongsTo
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $technologies = ['HTML', 'CSS', 'Javascript', 'VueJs', 'Php', 'Laravel'];
+
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->name = $technologies;
+            $new_technology->slug = Str::slug($new_technology->name);
+            $new_technology->save();
+        }
+    }
+}

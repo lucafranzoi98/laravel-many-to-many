a5d1a11488695cf6e7e183e6db1192c93a9343e5
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-16 09:42:12 +0100

    fix database issues

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,7 +21,9 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            ProjectSeeder::class
+            ProjectSeeder::class,
+            TypeSeeder::class,
+            TechnologySeeder::class
         ]);
     }
 }
